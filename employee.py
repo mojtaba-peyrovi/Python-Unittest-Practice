9e1ea2c098e6bdef907b44244f8607e6c7a6ba16
Mojtaba Peyrovi <mojtaba@heroleads.com>
2020-07-02 14:21:01 +0700

    finished the firt tutorial

--- employee.py
+++ employee.py
@@ -4,6 +4,8 @@ Created on Thu Jun 11 00:09:25 2020
 
 @author: hero144
 """
+import requests
+
 raise_amt = 1.05
 
 class Employee:
@@ -26,11 +28,23 @@ class Employee:
         self.pay = int(self.pay * raise_amt)
     
 
+    def monthly_schedule(self, month):
+        response = requests.get(f'http://company.com/{self.last}/{month}')
+        if response.ok:
+            return response.text
+        else:
+            return "Bad Response!"
+
+
+
+
+
 Employee_1 = Employee('mojtaba','peyrovi',20000)
         
         
-print(Employee.email(Employee_1),Employee.fullname(Employee_1),Employee.apply_raise(Employee_1))
+# print(Employee.email(Employee_1),Employee.fullname(Employee_1),Employee.apply_raise(Employee_1))
 # Employee.fullname(Employee_1)
 
 
 
+    
--- test_employee.py
+++ test_employee.py
@@ -6,6 +6,7 @@ Created on Thu Jun 11 01:10:20 2020
 """
 
 import unittest
+from unittest.mock import patch
 from employee import Employee
 
 
@@ -17,8 +18,7 @@ class TestEmployee(unittest.TestCase):
     
     def tearDown(self):
         pass
-    
-    
+     
     
     def test_email(self):
         # emp_1 = Employee('Corey','Schafer',50000)
@@ -32,6 +32,24 @@ class TestEmployee(unittest.TestCase):
         
         self.assertEquals(self.emp_1.email(), 'John.Schafer@email.com')
         self.assertEqual(self.emp_2.email(), 'Sue.arndt@email.com')
+        
+    def test_monthly_schedule(self):
+        # test successful response
+        with patch('employee.requests.get') as mock_get:
+            mock_get.return_value.ok = True
+            mock_get.return_value.text = 'Success'
+            
+            schedule = self.emp_1.monthly_schedule('May')
+            mock_get.assert_called_with('http://company.com/Schafer/May')
+            self.assertEqual(schedule, 'Success')
+       # test failed response
+        with patch('employee.requests.get') as mock_get:
+            mock_get.return_value.ok = False
+
+            schedule = self.emp_2.monthly_schedule('Jun')
+            mock_get.assert_called_with('http://company.com/Smith/Jun')
+            self.assertEqual(schedule, 'Bad Response!')     
+        
         
 if __name__ == "__main__":
     unittest.main()        
